1c0dca98665bda3fda403f481ab6a44597a22cd8
apisityaj <apisity@iottechgroup.com>
2024-05-28 14:04:13 +0700

    update navbar

--- src/app/_component/layouts/navbar.tsx
+++ src/app/_component/layouts/navbar.tsx
@@ -17,11 +17,19 @@ const Navbar = () => {
       <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center rounded-full bg-white shadow-gray-950 justify-between h-16">
           {links.map((link, index) => (
-            <div key={index} className="relative flex justify-center items-center pl-2 pr-2 h-2/3">
+            <div
+              key={index}
+              className="relative flex justify-center items-center pl-2 pr-2 h-2/3"
+            >
               <Link
                 key={index}
                 href={link.url}
-                className={`${currentPage === link.text ? 'bg-red-500 text-white font-bold' : 'bg-white text-[#212121]'} px-3 py-2 rounded-full text-sm font-medium`}
+                onClick={() => setCurrentPage(link.text)}
+                className={`${
+                  currentPage === link.text
+                    ? "bg-red-500 text-white font-bold"
+                    : "bg-white text-[#212121]"
+                } px-3 py-2 rounded-full text-sm font-medium`}
               >
                 {link.text}
               </Link>
